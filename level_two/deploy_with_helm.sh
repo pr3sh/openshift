f13405b8542912c0a6077ffd5b832470e0baa330
Precious Chima <precious_chima@hotmail.com>
2021-10-06 10:27:41 -0400

    work on Helm Charts

--- level_two/deploy_with_helm.sh
+++ level_two/deploy_with_helm.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Install Tree.
-$ sudo apt install tree
+sudo apt install tree
 
 # Create context directory and display Tree structure.
-$ EXPORT BUILD_DIR && tree $BUILD_DIR
+EXPORT BUILD_DIR && tree $BUILD_DIR
 
 # Update dependencies for the chart.
-$ helm dependency update
+helm dependency update
+
+
+# Define more environment variables
+EXPORT OCP_DEV_USER
+EXPORT OCP_DEV_PASS
+EXPORT OCP_MASTER_API
+EXPORT PROJECT_NAME
+EXPORT APP_NAME
+
+oc login -u $OCP_DEV_USER -p $OCP_DEV_PASS --server $OCP_MASTER_API && \
+	oc new-project $PROJECT_NAME
+
+# Deploy to application in RHOCP Cluster.
+helm install $APP_NAME $BUILD_DIR
+
+# Display deployments
 
 
