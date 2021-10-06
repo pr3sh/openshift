a5387c0368f9e0bb265416523201106fd1f219d2
Precious Chima <precious_chima@hotmail.com>
2021-10-06 10:23:40 -0400

    work on Helm Charts

--- level_two/deploy_with_helm.sh
+++ level_two/deploy_with_helm.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Install Tree
+# Install Tree.
 $ sudo apt install tree
 
-# Create context directory and display Tree structure
+# Create context directory and display Tree structure.
 $ EXPORT BUILD_DIR && tree $BUILD_DIR
+
+# Update dependencies for the chart.
+$ helm dependency update
 
 
