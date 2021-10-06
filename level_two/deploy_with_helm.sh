c41acf6fdc7170cb4718e9f74a9de415fc093f72
Precious Chima <precious_chima@hotmail.com>
2021-10-06 10:31:22 -0400

    work on Helm Charts

--- level_two/deploy_with_helm.sh
+++ level_two/deploy_with_helm.sh
@@ -16,6 +16,7 @@ EXPORT OCP_DEV_PASS
 EXPORT OCP_MASTER_API
 EXPORT PROJECT_NAME
 EXPORT APP_NAME
+EXPORT SVC_URL
 
 oc login -u $OCP_DEV_USER -p $OCP_DEV_PASS --server $OCP_MASTER_API && \
 	oc new-project $PROJECT_NAME
@@ -24,5 +25,13 @@ oc login -u $OCP_DEV_USER -p $OCP_DEV_PASS --server $OCP_MASTER_API && \
 helm install $APP_NAME $BUILD_DIR
 
 # Display deployments
+oc get deployments && oc get pods
+
+# Expose service and test endpoint.
+oc  expose service $APP_NAME 
+
+SVC_URL=$(oc get route -n $PROJECT_NAME $APP_NAME -o jsonpath='{.spec.host}{"\n"}')
+
+echo testing database service endpoint....
 
 
